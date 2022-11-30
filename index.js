cd850f836855ff5873628e51019f598df5f02047
Seunggyu <seunggyul14@icloud.com>
2022-11-29 17:39:44 -0800

    commit tree c1fff49d645b36e1d5e8191095b5b74324724d2e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, expression) {
+    const cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${expression} gift!`);
+    }
+    return cards;
+}
+console.log(writeCards(names, 'surprise'));
+
+function countDown(number) {
+    for (let countDown = number; countDown >= 0; countDown--){
+        console.log(countDown);
+    }
+}
+
